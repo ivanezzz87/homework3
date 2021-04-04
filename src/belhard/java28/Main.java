d79276e8edda13f05d503498b0c2789731d57326
”Ivan <mseptk@yandex.ru>
2021-04-04 22:59:13 +0300

    добавлен код вывода пор года

--- src/belhard/java28/Main.java
+++ src/belhard/java28/Main.java
@@ -3,6 +3,15 @@ package belhard.java28;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int monthNumber = 4;
+	if (monthNumber == 12 || monthNumber == 1 ||monthNumber == 2) {
+        System.out.println("Зима");
+    } else if (monthNumber == 3 || monthNumber == 4 ||monthNumber == 5) {
+        System.out.println("Весна");
+    } else if (monthNumber == 6 || monthNumber == 7 ||monthNumber == 8) {
+        System.out.println("Лето");
+    } else {
+        System.out.println("Осень");
+    }
     }
 }

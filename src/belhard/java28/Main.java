e50bfdc06d8af19a20cb36c387afad690374bb6a
”Ivan <mseptk@yandex.ru>
2021-04-05 00:22:13 +0300

    вывод двумерного массива

--- src/belhard/java28/Main.java
+++ src/belhard/java28/Main.java
@@ -17,5 +17,24 @@ public class Main {
 	for (int i = myArr.length-1;i >= 0; i--) {
             System.out.println(myArr[i]);
         }
+	int [][] squareArray = new int [3][4];
+	squareArray[0][0] = 13;
+	squareArray[0][1] = 9;
+	squareArray[0][2] = 1;
+	squareArray[0][3] = 6;
+	squareArray[1][0] = 1;
+	squareArray[1][1] = 3;
+	squareArray[1][2] = 33;
+	squareArray[1][3] = 14;
+	squareArray[2][0] = 4;
+	squareArray[2][1] = 15;
+	squareArray[2][2] = 0;
+	squareArray[2][3] = 20;
+
+    for (int i = 0; i < 3; i++) {
+       for (int j = 0; j < 4; j++) {
+           System.out.println(squareArray[i][j] + "[" + i + "]" + "[" + j + "]");
+       }
+    }
     }
 }
